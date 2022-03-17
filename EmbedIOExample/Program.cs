ce59ad48f6c5b4c870c5a6570c9f69fb94d76759
hasan.akpolad <hasan.akpolad@detaysoft.com>
2022-03-17 15:40:38 +0300

    Add project files.

---
+++ EmbedIOExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace EmbedIOExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Host.CreateDefaultBuilder(args).Build().Run();
+        }
+    }
+}
+
